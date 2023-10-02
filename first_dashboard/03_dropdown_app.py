884dd0c98b0106e0114537e87a23a2f0e4240e9d
jakubdaron <254151@student.pwr.edu.pl>
2023-10-02 12:58:15 +0200

    Update aplikacji typu Dropdown

--- first_dashboard/03_dropdown_app.py
+++ first_dashboard/03_dropdown_app.py
@@ -23,3 +23,22 @@ app.layout = html.Div([
         id='graph-1'
     )
 ])
+
+@app.callback(
+    Output('graph-1', 'figure'),
+    [Input('drop-1', 'value')]
+)
+def update_graph(value):
+    print(value)
+    data_dict = {
+        'PL': [3, 2, 5, 4, 7, 2, 4],
+        'GER': [4, 1, 3, 4, 2, 4, 1]
+    }
+    return {'data': [
+        {'y': data_dict[value],
+         'type': 'scatter',
+         'fill': 'tozeroy'}
+    ]}
+
+if __name__ == '__main__':
+    app.run_server(debug=True, port=8081)
